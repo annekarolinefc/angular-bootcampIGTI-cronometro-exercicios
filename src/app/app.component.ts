a436e0f1e85d7c46124cb4931e23db42922d23ae
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-24 08:29:36 -0300

    Criando a mudança de estado - simples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cronometro-exercicios';
+  /**1º ESTADO: propriedade de controle BOOLEANA - AO A CONFIGURAÇÃO ESTA ABERTA OU NAO */
+  config: boolean = false;
 }
